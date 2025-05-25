b698d3a3279cf0a334bd8c13167c343508e2548a
Josue Borges <Josue.diniz.2002@gmail.com>
2025-05-25 13:26:56 -0300

    Class ItemBiblioteca implementada

--- BibliotecaSimplesApp/Models/ItemBiblioteca.cs
+++ BibliotecaSimplesApp/Models/ItemBiblioteca.cs
@@ -1,12 +1,37 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using BibliotecaSimplesApp.Models;
 
-namespace BibliotecaSimplesApp.Models
+namespace BibliotecaSimples.Models
 {
-    internal class ItemBiblioteca
+    public abstract class ItemBiblioteca
     {
+        private static int proximoIdItem = 1;
+        public int Id { get; private set; }
+        public string Titulo { get; set; }
+        public int AnoPublicacao { get; set; }
+
+        public int CategoriaId { get; set; }
+        public Categoria Categoria { get; set; }
+
+        protected ItemBiblioteca(string titulo, int anoPublicacao, Categoria categoria)
+        {
+            Id = proximoIdItem++;
+            Titulo = titulo;
+            AnoPublicacao = anoPublicacao;
+
+            if (categoria == null)
+            {
+                throw new ArgumentNullException(nameof(categoria), "A categoria não pode ser nula para um ItemBiblioteca.");
+            }
+            Categoria = categoria;
+            CategoriaId = categoria.Id;
+        }
+
+        public abstract string ExibirDetalhes();
+
+        public override string ToString()
+        {
+            return $"{Id}: {Titulo} ({AnoPublicacao}) - Cat: {Categoria?.Nome ?? "N/A"}";
+        }
     }
 }
