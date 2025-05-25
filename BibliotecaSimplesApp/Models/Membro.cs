9ea7db67ea14643b531a3265e657fcf94eb01448
Josue Borges <Josue.diniz.2002@gmail.com>
2025-05-25 13:35:29 -0300

    class Membro implementado

--- BibliotecaSimplesApp/Models/Membro.cs
+++ BibliotecaSimplesApp/Models/Membro.cs
@@ -1,12 +1,27 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Collections.Generic;
+using BibliotecaSimplesApp.Models;
 
-namespace BibliotecaSimplesApp.Models
+namespace BibliotecaSimples.Models
 {
-    internal class Membro
+    public class Membro
     {
+        private static int proximoIdMembro = 1;
+        public int Id { get; private set; }
+        public string Nome { get; set; }
+        public string Email { get; set; }
+        public List<Emprestimo> Emprestimos { get; private set; }
+
+        public Membro(string nome, string email)
+        {
+            Id = proximoIdMembro++;
+            Nome = nome;
+            Email = email;
+            Emprestimos = new List<Emprestimo>();
+        }
+
+        public override string ToString()
+        {
+            return $"{Id}: {Nome} ({Email})";
+        }
     }
 }
