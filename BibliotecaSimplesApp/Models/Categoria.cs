c304313f0736c0ca9eb1a8e14c8cb1a95b688905
Josue Borges <Josue.diniz.2002@gmail.com>
2025-05-25 13:30:28 -0300

    class Categoria implementado

--- BibliotecaSimplesApp/Models/Categoria.cs
+++ BibliotecaSimplesApp/Models/Categoria.cs
@@ -1,12 +1,37 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Collections.Generic;
 
-namespace BibliotecaSimplesApp.Models
+namespace BibliotecaSimples.Models
 {
-    internal class Categoria
+    public class Categoria
     {
+        private static int proximoIdCategoria = 1;
+        public int Id { get; private set; }
+        public string Nome { get; set; }
+        public List<ItemBiblioteca> Itens { get; private set; }
+
+        public Categoria(string nome)
+        {
+            Id = proximoIdCategoria++;
+            Nome = nome;
+            Itens = new List<ItemBiblioteca>();
+        }
+
+        public void AdicionarItem(ItemBiblioteca item)
+        {
+            if (item != null && !Itens.Contains(item))
+            {
+                Itens.Add(item);
+                if (item.Categoria != this)
+                {
+                    item.Categoria = this;
+                    item.CategoriaId = this.Id;
+                }
+            }
+        }
+
+        public override string ToString()
+        {
+            return $"{Id}: {Nome}";
+        }
     }
 }
